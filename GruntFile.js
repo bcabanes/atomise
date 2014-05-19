289dc946182a21d6327f2a311150f6fb36ba36a4
bcabanes <b.cabanes@novactive.com>
2014-05-18 21:21:30 -0400

    develop: add handle of scss/SASS files

--- GruntFile.js
+++ GruntFile.js
@@ -6,70 +6,84 @@ module.exports = function(grunt) {
 
         /** GENERATE JSON PATTERNS LIBRARY **/
         dir2strings: {
-            options: {
-                exclude: '**/*.md'
-            },
-            data: {
-              root: 'public/patternlab/sources/_patterns',
-              dest: 'public/patternlab/sources/patterns.json'
-            },
+          options: {
+            exclude: '**/*.md'
+          },
+          data: {
+            root: 'public/patternlab/sources/_patterns',
+            dest: 'public/patternlab/sources/patterns.json'
+          },
+        },
+
+        sass: {
+          options: {
+            // includePaths: [''],
+            outputStyle: 'compressed', // 'nested' (default), 'expanded', 'compact', 'compressed'
+            sourceComments: 'map',
+            // sourceMap: 'app/css/app.css.map'
+          },
+          dist: {
+            files: {
+              'public/css/main.css': 'public/scss/main.scss'
+            }
+          }
         },
 
         /** Warch for files modification **/
         watch: {
-            grunt: {
-                files: ['Gruntfile.js'],
-                // tasks: ['sass']
-            },
-            sass: {
-                files: 'public/scss/**/*.scss',
-                tasks: ['sass']
-            },
-            livereload: {
-                // files: ['public/*.html', 'public/js/{,**/}*.js', 'public/css/{,*/}*.css', 'public/images/{,*/}*.{jpg,gif,svg,jpeg,png}'],
-              files: [
-                'public/patternlab/{,**/}*.{js, mustache}',
-                'public/*.html',
-                'public/js/{,**/}*.js',
-                'public/css/{,*/}*.css',
-                'public/images/{,*/}*.{jpg,gif,svg,jpeg,png}'
-              ],
-              options: {
-                livereload: true
-              }
+          grunt: {
+            files: ['Gruntfile.js'],
+            tasks: ['sass']
+          },
+          sass: {
+            files: 'public/scss/{,**/}*.scss',
+            tasks: ['sass']
+          },
+          livereload: {
+            // files: ['public/*.html', 'public/js/{,**/}*.js', 'public/css/{,*/}*.css', 'public/images/{,*/}*.{jpg,gif,svg,jpeg,png}'],
+            files: [
+              'public/patternlab/{,**/}*.{js, mustache}',
+              'public/*.html',
+              'public/js/{,**/}*.js',
+              'public/css/{,*/}*.css',
+              'public/images/{,*/}*.{jpg,gif,svg,jpeg,png}'
+            ],
+            options: {
+              livereload: true
             }
+          }
         },
 
         /** Create servers **/
         connect: {
-            app: {
-                options: {
-                    hostname: 'patternlab.dev',
-                    port: 9000,
-                    base: 'public/',
-                    livereload: true
-                }
-            },
-            dist: {
-                options: {
-                    hostname: 'patternlab.dev',
-                    port: 9001,
-                    base: 'dist/',
-                    keepalive: true
-                }
+          app: {
+            options: {
+              hostname: 'patternlab.dev',
+              port: 9000,
+              base: 'public/',
+              livereload: true
             }
+          },
+          dist: {
+            options: {
+              hostname: 'patternlab.dev',
+              port: 9001,
+              base: 'dist/',
+              keepalive: true
+            }
+          }
         },
 
         /** OPEN BROWER **/
         open: {
-            app: {
-                path: 'http://patternlab.dev:9000/',
-                app: 'Google Chrome'
-            },
-            dist: {
-                path: 'http://patternlab.dev:9001/',
-                app: 'Google Chrome'
-            }
+          app: {
+            path: 'http://patternlab.dev:9000/',
+            app: 'Google Chrome'
+          },
+          dist: {
+            path: 'http://patternlab.dev:9001/',
+            app: 'Google Chrome'
+          }
         }
 
     });
