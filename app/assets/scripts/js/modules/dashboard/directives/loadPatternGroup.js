302898001e7fbbc33fdb467de1c8ff192e3b3e7a
bcabanes <benjamin.cabanes@cossette.com>
2015-01-07 17:36:39 -0500

    add load pattern group directive

---
+++ app/assets/scripts/js/modules/dashboard/directives/loadPatternGroup.js
@@ -0,0 +1,33 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('app.dashboard')
+        .directive('loadPatternGroup', directive);
+
+    directive.$inject = [];
+
+    function directive() {
+        return {
+            'restrict': 'E',
+            'scope': {
+                'patterns': '='
+            },
+            'templateUrl': '/assets/partials/dashboard/directives/loadPatternGroup.html',
+            'link': link
+        };
+
+        function link(scope, element, attrs) {
+            scope.groups = getGroups();
+
+            function getGroups() {
+                var groups = [];
+                for(var i in scope.patterns) {
+                    groups.push(i);
+                }
+                return groups;
+            }
+        }
+    }
+
+})(angular);
