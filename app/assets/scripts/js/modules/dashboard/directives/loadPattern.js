1b6a81181176f52552209e6361bf717ce79e1751
bcabanes <benjamin.cabanes@cossette.com>
2015-01-12 11:11:36 -0500

    Template are rendered by MustacheJS and settings can be passed in

--- app/assets/scripts/js/modules/dashboard/directives/loadPattern.js
+++ app/assets/scripts/js/modules/dashboard/directives/loadPattern.js
@@ -6,9 +6,9 @@
     .module('app.dashboard')
     .directive('loadPattern', directive);
 
-    directive.$inject = ['$http', '$sce'];
+    directive.$inject = ['$http', '$sce', '$q'];
 
-    function directive($http, $sce) {
+    function directive($http, $sce, $q) {
         return {
             'restrict': 'E',
             'scope': {
@@ -20,19 +20,44 @@
 
         function link(scope, element, attrs) {
             scope.template = '';
+            scope.templateTemp = '';
+            scope.jsonPath = scope.path.split('.')[0]+'.json';
+            scope.settings = {};
+
+            scope.templatePromise = $q.defer();
+            scope.settingsPromise = $q.defer();
 
             $http.get('/sources/_patterns/' + scope.path)
                 .success(function(data) {
-                    scope.template = $sce.trustAsHtml(rendering(data));
+                    scope.templateTemp = data;
+                    scope.templatePromise.resolve();
                 })
-                .error(function() {
+                .error(function() {
                     console.error('Error: ', 'The pattern can not be found.');
                 });
 
+            $http.get('/sources/_patterns/' + scope.jsonPath)
+                .success(function(data) {
+                    scope.settings = data;
+                    scope.settingsPromise.resolve();
+                })
+                .error(function() {
+                    scope.settingsPromise.resolve();
+                    console.info('Notice: ', 'The settings\' pattern can not be found.');
+                });
 
-            function rendering(template) {
-                // return Mustache.render(template);
-                return template;
+            /**
+             * Wait all promises to be resolved before
+             * rendering template with settings.
+             */
+            $q.all([scope.templatePromise.promise, scope.settingsPromise.promise])
+                .then(function() {
+                    scope.template = $sce.trustAsHtml(rendering(scope.templateTemp, scope.settings));
+                });
+
+            function rendering(template, settings) {
+                return Mustache.render(template, settings);
+                // return template;
             }
 
         }
