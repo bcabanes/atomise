ee949b2eaa0d293f5ce8818086029e77c24ce5c4
bcabanes <contact@benjamincabanes.com>
2015-01-24 15:13:33 -0500

    add navigation panel and navigation list styles

--- app/assets/scripts/js/modules/layout/controllers/header.js
+++ app/assets/scripts/js/modules/layout/controllers/header.js
@@ -5,16 +5,18 @@
         .module('app.layout')
         .controller('layout.HeaderController', controller);
 
-    controller.$inject = ['pattern.Factory'];
+    controller.$inject = ['pattern.Factory', '$rootScope'];
 
-    function controller(pattern) {
+    function controller(pattern, $rootScope) {
         /* jshint validthis: true */
         var vm = this;
+        vm.showNavigationPanel = false;
 
         /**
          * Get the pattern tree to buil the header menu
          */
         vm.patternsTree = pattern.getTree();
+
     }
 
 })(angular);
